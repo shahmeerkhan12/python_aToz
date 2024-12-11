8b149784b4f3286197844f0fd7c70912604bacb0
Shahmir Khan <kshahmir822@gmail.com>
2024-12-11 08:42:18 +0500

    python data types! these are very simple

---
+++ 1DataTypes.py
@@ -0,0 +1,42 @@
+# # # name = "Ahmad Ali"
+# # # print("hello! "+ name)
+# # # print(type(name))
+
+# # age=32
+# # age=age+1
+# # print(age)
+# # print("your age is: "+str(age))
+# # #print(type(age))will print the type of age var
+
+# # # other data types are bool and float
+
+# # #tutorial 2
+
+# # #multiple assignments
+ali_age, Usman_age = 23,24
+print(str(ali_age)+" "+str(Usman_age))
+
+# # # let if we have mulitple variables with the same values
+# # length = height = width = 5
+# # print("length, height and width are: " + str(length)+" "+ str(height) +" "+ str(width))
+
+# #tutorial 3 Various string functions
+
+# name = "shahmir khan"
+# print(name.capitalize()) #if there is a space in your string then just the first letter will be capitalized
+# print(name.upper()) # lower also works
+# print(name.find("o"))#will return -1 b/c it doesn't exist
+# print(name.find("s"))
+# print(name.find("r"))
+# # other string methods are much more like "isdigit",isalpha, len,lower, count, replace, etc
+
+#tutorial 04 Casting
+
+y=(float(2)) #int
+x=float((3.0)) #float
+s=float(("4"))#string
+
+print(y)
+print(x) # or you can do it permanently by x=int(x)
+print(s*3)
+
