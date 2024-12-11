34b0e13c06e3e40ad950e9f4012287428b4336e7
Shahmir Khan <kshahmir822@gmail.com>
2024-12-11 08:59:51 +0500

    how to take inputs from the user

---
+++ 2inputMethods.py
@@ -0,0 +1,9 @@
+#tutorial 05
+name = input("What is your name?:")
+age = str(input("What is your age?:"))# note type conversion or casting
+
+# age=age+1
+
+print("Hello! "+ name)
+print("You are "+(age)+" old")
+   
