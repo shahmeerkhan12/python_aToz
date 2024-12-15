5df629ef20344ad7839f6896a87538589756a412
Shahmir Khan <kshahmir822@gmail.com>
2024-12-15 12:47:35 +0500

    list creation is simple in python! No heavy and complex syntax

---
+++ 6_list/creating_list.py
@@ -0,0 +1,16 @@
+# a list of strings
+sports = ["Football","Hockey","Cricket","Volleyball"]
+
+# a list of integers(numbers)
+evens = [2,4,6,8,10,12,14]
+
+# a list of characters
+grades = ['A','B','C','D','F']
+
+# printing the list
+
+print(sports)
+
+print(evens)
+
+print(grades)
