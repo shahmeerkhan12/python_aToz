3eccadbea618f2d1cc88c579e4d2c0bc0054ea54
Shahmir Khan <kshahmir822@gmail.com>
2024-12-20 15:14:39 +0500

    LIST in python and operations performed on it

---
+++ list_in_python/creating_list.py
@@ -0,0 +1,14 @@
+# a list of strings
+sports = ["Football","Hockey","Cricket","Volleyball"]
+
+# printing the list elements using for loop
+for i in sports:
+   print(i)
+
+
+# a list of integers(numbers)
+evens = [2,4,6,8,10,12,14]
+
+# a list of characters
+grades = ['A','B','C','D','F']
+
---
+++ list_in_python/list_operations.py
@@ -0,0 +1,36 @@
+# performing some useful operations on lists
+
+from operator import truediv
+from pickle import TRUE
+
+
+sports = ["Football","Hockey","Cricket","Volleyball"]
+
+# adding new elements
+sports[1]="Rugbey"
+
+# adding element to the end of the list
+sports.append("Running")
+
+# some more built-in methods on list
+
+sports.pop() # removes the last element
+
+sports.remove("Football") #removes the value
+
+sports.sort() # will sort the list in ascending order
+
+# if you want to sort in descending order then use following
+
+sports.sort(reverse=True)
+
+sports.reverse() # reverses the array
+
+sports.clear() # REMOVES all the list elements
+
+
+#printing each element in the list by for loop
+
+for a in sports :
+   print(a,end=" ")
+print()
