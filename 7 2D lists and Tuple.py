af19ce3f20b880317ad62e66fec202a63fc0135f
Shahmir Khan <kshahmir822@gmail.com>
2024-12-21 09:35:05 +0500

    2d dimensional lists and tuple in python

---
+++ 7 2D lists and Tuple.py
@@ -0,0 +1,62 @@
+ 
+# drinks = ["Tea", "Coffee", "GreenTea"]
+# dinner = ["Pizza", "ChikenSoup", "ChineseRice"]
+# dessert = ["Ice Cream", "Cake", "Backery"]
+
+
+# gallery = [
+#             ["Tea", "Coffee", "GreenTea"],
+#             ["Pizza", "ChikenSoup", "ChineseRice"],
+#             ["Ice Cream", "Cake", "Backery"]
+#           ]
+
+# print(gallery[1][0])
+
+# #To create a 2D list of these
+
+# Food = [drinks,dinner,dessert]
+
+# # printing the 2D list
+
+# print(Food)
+
+# # printing Random elements from the list
+
+# # print(Food[0]) #printing the first list 
+# # print(Food[1]) #printing the second list
+# # print(Food[2]) #printing the last list
+
+# # printing a specific element from the list
+
+# #print(Food[0][1])   [rows][columns]
+
+# # we can also use a for loop to print the 2D list
+
+# # What is tuple: a collection of ordered and unchangeable, that is used to group related data
+
+# student = ("ahmad",19,"male")
+# # u cannot do operations like insert, append, pop and other operations that 
+
+# #print(student[2])
+
+# print("printing tuple data")
+# print()
+# for x in student:
+#    print(x, end=" ") #printin the output in the single line
+
+# print()
+
+# if "ahmad" in student :
+#    print("ahmad is present")
+
+# revising
+# lists
+sports = ["hockey","cricket","football"]
+# print (sports)
+for i in sports:
+   print(i)
+# tuple
+# printing the tuple
+items = ("pasta","FruitChart","juice")
+for item in items:
+   print(item)
