d6373cf20e508b0f6a1d43fd90c37eead4ddd3ce
Shahmir Khan <kshahmir822@gmail.com>
2025-01-08 21:26:29 +0500

    warlus operators are really useful

--- OOP_in_python/30 warlus_operator.py
+++ OOP_in_python/30 warlus_operator.py
@@ -1,11 +1,13 @@
 # warlus operator ( := )
 # for quick assignments to variables
-# an alternative assignment operator in python
-# menu = list()
-# while food:=input("what food do you like: ") != "quit":
-#    menu.append(food) 
+
 print(happpy:=True)
 
 (First := input("please enter ur first name: ") !=" ")
+
 (last := input("please enter ur last name: ") !=" ")
-(age := input("please enter ur age : ") !=" ")
+
+# an  assignment operator in python
+menu = list()
+while food:=input("what food do you like: ") != "quit":
+   menu.append(food) 
